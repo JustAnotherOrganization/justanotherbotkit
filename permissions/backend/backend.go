bb82cac04168ec86eea0699fb29095bf689b87de
Grim <GrimTheReaper@users.noreply.github.com>
2017-12-11 11:17:23 -0600

    Makes it be a bit more clear and cut
    Just a cosmetic change to clarify the paramaters and returned values.

--- permissions/backend/backend.go
+++ permissions/backend/backend.go
@@ -3,9 +3,9 @@ package backend
 type (
 	// DB provides access to the underlying database.
 	DB interface {
-		WriteUser(id, name string) error
-		CheckUser(string) (bool, error)
-		GetPerms(string) ([]string, error)
-		SetPerms(string, ...string) error
+		WriteUser(userID, name string) error
+		CheckUser(userID string) (hasPermission bool, err error)
+		GetPerms(userID string) (permissions []string, err error)
+		SetPerms(userID string, permissions ...string) error
 	}
 )
